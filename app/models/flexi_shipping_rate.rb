112c70a9d7f285b3b0cc6a08f447c61b4decf4d3
Brian Quinn <briandquinn@gmail.com>
2009-01-15 15:50:08 +0000

    Updatd extension to support upcoming Spree 0.6.0 release

--- app/models/flexi_shipping_rate.rb
+++ app/models/flexi_shipping_rate.rb
@@ -4,6 +4,6 @@ class FlexiShippingRate < ActiveRecord::Base
   belongs_to :zone
   
   validates_presence_of :first_item_price, :max_items, :shipping_category, :zone
-  validates_uniqueness_of :shipping_category_id, :scope => :zone_id, :message => " already exists for the specified Zone."
+  validates_uniqueness_of :shipping_category_id, :scope => :zone_id 
   
 end
--- flexi_rate_shipping_extension.rb
+++ flexi_rate_shipping_extension.rb
@@ -29,7 +29,7 @@ class FlexiRateShippingExtension < Spree::Extension
     Admin::ConfigurationsController.class_eval do
       before_filter :add_flexi_rate_links, :only => :index
       def add_flexi_rate_links
-        @extension_links << {:link => admin_flexi_shipping_rates_path, :link_text => Globalite.localize(:ext_flexi_shipping_rates), :description => Globalite.localize(:ext_flexi_shipping_rates_description)}
+        @extension_links << {:link => admin_flexi_shipping_rates_path, :link_text => FlexiShippingRate.human_name(:count => 2), :description => t('flexi_shipping_rates_description')}
       end
     end
   end
